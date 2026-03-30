7da492c2f1ce06e391da7177be4a2535902998a5
Savage81-web <st10516548@myemeris.edu.za>
2026-03-30 08:55:56 +0200

    Commet 01; addimg project files
    empty project has been added.

---
+++ src/main/java/com/mycompany/switch_statements/Switch_Statements.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.switch_statements;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Switch_Statements {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
